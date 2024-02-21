f81f6cfa7b51f2e2a1a3b9de2a835ed0504372d9
Rafael Carvalho <140760825+carvlhu@users.noreply.github.com>
2024-02-21 13:28:19 -0300

    Projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Header } from './components/Header'
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+      <Header title="Projeto React" subTitle="Boa Boa"/>
+      <main className='container'>
+        <h1>Conteúdo do projeto React</h1>
+
+        <div>
+        <a href="#">Vai para Contato</a>
+        </div>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import "./style.css";
+
+export function Header (props) {
+
+    return (
+        
+        <header>
+            <div className="container">
+                <h1>{props.title}</h1>
+                <h2>{props.subTitle}</h2>
+            </div>
+            
+        </header>
+    )
+}
+
